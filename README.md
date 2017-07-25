da26f4f9546b11c71a5f5093618e5ea661dc0322
Luis Felipe Sánchez <lfelipe1501@gmail.com>
2017-07-25 16:55:27 -0500

    Arch-MOTD
    These scripts look great for your MOTDS on Arch Linux systems

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-Arch-MOTD
+ _____________________________________
+/ MOTD for Arch Linux Systems         \
+| This repository is for you Arch :heart: |
+\_____________________________________/
+   \
+    \
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+	
+![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/lfelipe1501/Atomic-Yakuake/issues)
+
+These scripts look great for your MOTDS on Arch Linux systems
+
+### Contact / Social Media
+
+*Get the latest News about Web Development, Open Source, Tooling, Server & Security*
+
+[![Twitter](https://github.frapsoft.com/social/twitter.png)](https://twitter.com/lfelipe1501)
+[![Facebook](https://github.frapsoft.com/social/facebook.png)](https://www.facebook.com/lfelipe1501)
+[![Github](https://github.frapsoft.com/social/github.png)](https://github.com/lfelipe1501)
+
+### Development by
+
+Developer / Author: [Luis Felipe Sánchez](https://github.com/lfelipe1501)
+Company: [lfsystems](https://www.lfsystems.xyz)
---
+++ update_motd.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#Script to update motd with relevant information.
+
+#Define output file
+motd="/etc/motd"
+
+# Collect information
+#USERNAME=`$USER`
+HOSTNAME=`uname -n`
+KERNEL=`uname -r`
+CPU=`awk -F '[ :][ :]+' '/^model name/ { print $2; exit; }' /proc/cpuinfo`
+ARCH=`uname -m`
+PACMAN=`checkupdates | wc -l`
+DISC=`df -h | grep /dev/xvda1 | awk '{print $5 }'`
+MEMORY1=`free -t -m | grep "Mem" | awk '{print $6" MB";}'`
+MEMORY2=`free -t -m | grep "Mem" | awk '{print $2" MB";}'`
+#PSA=`ps -Afl | wc -l`
+#PSU=`ps U $USER h | wc -l`
+
+#Time of day
+HOUR=$(date +"%H")
+if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
+then   TIME="Morning"
+elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
+then   TIME="Afternoon"
+else   TIME="Evening"
+fi
+
+#System uptime
+uptime=`cat /proc/uptime | cut -f1 -d.`
+upDays=$((uptime/60/60/24))
+upHours=$((uptime/60/60%24))
+upMins=$((uptime/60%60))
+upSecs=$((uptime%60))
+
+
+#System load
+LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+
+#Color variables
+#W="\033[00;37m"
+W="\033[0m"
+B="\033[01;36m"
+R="\033[01;31m"
+G="\033[01;32m"
+N="\033[0m"
+
+#Clear screen before motd
+cat /dev/null > $motd
+
+echo -e "
+        $B. $W
+       $B/#\ $W                     _     $B _ _
+      $B/###\ $W      __ _ _ __ ___| |__  $B| (_)_ __  _   ___  __ 
+     $B/#####\ $W    / _' | '__/ __| '_ \ $B| | | '_ \| | | \ \/ /
+    $B/##.-.##\ $W  | (_| | | | (__| | | |$B| | | | | | |_| |>  <  
+   $B/##(   )##\ $W  \__,_|_|  \___|_| |_|$B|_|_|_| |_|\__._/_/\_\\
+  $B/#.--   --.#\ $W
+ $B/'           '\ 
+" > $motd
+echo -e "$G---------------------------------------------------------------" >> $motd
+echo -e "$W   Good $TIME$A You're Logged Into $B$A$HOSTNAME$W! " 	    >> $motd
+echo -e "$G---------------------------------------------------------------" >> $motd
+echo -e "$B    KERNEL $G:$W $KERNEL $ARCH                                 " >> $motd
+echo -e "$B       CPU $G:$W $CPU                                          " >> $motd
+echo -e "$B    MEMORY $G:$W $MEMORY1 / $MEMORY2                           " >> $motd
+echo -e "$B  USE DISK $G:$W $DISC                          	          " >> $motd
+echo -e "$G---------------------------------------------------------------" >> $motd
+echo -e "$B  LOAD AVG $G:$W $LOAD1, $LOAD5, $LOAD15		          " >> $motd
+echo -e "$B    UPTIME $G:$W $upDays days $upHours hours $upMins minutes $upSecs seconds " >> $motd
+#echo -e "$B PROCESSES $G:$W You are running $PSU of $PSA processes        " >> $motd
+echo -e "$B    PACMAN $G:$W $PACMAN packages can be updated               " >> $motd
+echo -e "$B     USERS $G:$W `users | wc -w` users logged in 	          " >> $motd
+echo -e "$G---------------------------------------------------------------" >> $motd
+echo -e "  $W Please do not share your login and use$G sudo$W for$R root$W access " >> $motd
+echo -e "$G---------------------------------------------------------------" >> $motd
+echo -e "$N" >> $motd
